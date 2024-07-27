389f10b657dba9e194f79fffb2e73709a836219e
nammalwarsai <nammalwarsai@gmail.com>
2024-07-27 19:16:36 +0530

    gsdf

--- src/components/Attendence.js
+++ src/components/Attendence.js
@@ -6,10 +6,6 @@ const Attendence = ({ hours, attendancePercentage }) => {
     <div>
       <div className="alert alert-dismissible alert-success">
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        <p>
-          Total hours attendend <strong>{hours}25</strong><br />
-          Attendance Percentage: <strong>{attendancePercentage}85%</strong>
-        </p>
         <a href="https://newerp.kluniversity.in/index.php?r=studentattendance%2Fstudentdailyattendance%2Fcourselist" className="alert-link">
           Click here to check your Attendance
         </a>
