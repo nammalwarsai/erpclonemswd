4cbe5d2489e44b9a9a6121477d73b9950a5380dc
nammalwarsai <nammalwarsai@gmail.com>
2024-08-05 13:14:35 +0530

    payment section updated

--- src/components/Paymentsdone.js
+++ src/components/Paymentsdone.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Paymentsdone = () => {
   return (
-    <div className="container text-center mt-5">
+    <div className="container text-center mt-7">
       <h1>Total Payments Done</h1>
+      <br/>
+      <h2>YOUR TOTAL PAYMENTS DONE <strong>4,00,000</strong></h2>
     </div>
   );
 };
