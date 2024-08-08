5be5ce13fb2efdd3d7c4436261a6a78cc542aa3f
nammalwarsai <nammalwarsai@gmail.com>
2024-08-08 16:10:23 +0530

    asdf

--- src/components/MERNTASK.js
+++ src/components/MERNTASK.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-
 const MERNTASK = () => {
   const [presentCounter, setPresentCounter] = useState(0);
-
   const students = [
     { id: '2300032191', name: 'Naidu Basudev', branch: 'CSE', year: '2nd Year', attendance: 'Present' },
     { id: '2300031724', name: 'Swayam Sreetam Swain', branch: 'CSE', year: '2nd Year', attendance: 'Present' },
@@ -11,13 +9,11 @@ const MERNTASK = () => {
     { id: '2300031724', name: 'Abhinandan', branch: 'CSE', year: '2nd Year', attendance: 'Present' },
     { id: '2300032190', name: 'B Ashok', branch: 'CSE', year: '2nd Year', attendance: 'Present' },
   ];
-
   const handleMarkPresent = () => {
     if (presentCounter < students.length) {
       setPresentCounter(presentCounter + 1);
     }
   };
-
   const handleMarkAbsent = () => {
     if (presentCounter > 0) {
       setPresentCounter(presentCounter - 1);
@@ -33,7 +29,8 @@ const MERNTASK = () => {
         {students.map((student, index) => (
           <div className="col-md-4 mb-4" key={index}>
             <div className={`card border-${student.attendance === 'Present' ? 'success' : 'danger'} mb-3`} style={{ maxWidth: '20rem' }}>
-              <div className="card-header">Header</div>
+              <div className="card-header">STUDENT DETAILS</div>
+              <div className="card-header"></div>
               <div className="card-body">
                 <h5 className="card-title">{student.name}</h5>
                 <p className="card-text"><strong>Student ID:</strong> {student.id}</p>
@@ -48,7 +45,9 @@ const MERNTASK = () => {
                 <button type="button" className="btn btn-outline-danger" onClick={handleMarkAbsent}>Mark Absent</button>
               </div>
             </div>
+            
           </div>
+          
         ))}
       </div>
     </div>
