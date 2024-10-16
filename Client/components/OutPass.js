d29fb332554da03278a1bed39e486a686820e3df
nammalwarsai <nammalwarsai@gmail.com>
2024-10-16 14:57:25 +0530

    changed the name to Client

---
+++ Client/components/OutPass.js
@@ -0,0 +1,14 @@
+// src/components/OutPass.js
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const OutPass = () => {
+  return (
+    <Container className="mt-5">
+      <h3>Apply for Out Pass</h3>
+      <p>Fill in the required information to apply for an out pass. This feature is coming soon.</p>
+    </Container>
+  );
+};
+
+export default OutPass;
